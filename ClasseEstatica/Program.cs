e89455d6401b80202f827b965924127a4d548abc
darlon vitor <darlonvitor@hotmail.com>
2020-05-22 10:45:04 -0300

    Exemplo de uso de uma Classe estática

---
+++ ClasseEstatica/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClasseEstatica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HerancaPolimorfismo.Adulto kid = new HerancaPolimorfismo.Adulto();
+            Console.WriteLine($"Soma de inteiros: {Calculadora.Somar(1,2)}");
+            Console.WriteLine($"Soma de doubles: {Calculadora.Somar(1,2.5)}");
+        }
+    }
+}
