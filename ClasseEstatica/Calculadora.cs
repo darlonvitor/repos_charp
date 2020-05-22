f23fb681c332e645f50550d6728250be1e7beb90
darlon vitor <darlonvitor@hotmail.com>
2020-05-22 10:43:21 -0300

    Classe estatica que realiza a soma de dois numeros

---
+++ ClasseEstatica/Calculadora.cs
@@ -0,0 +1,12 @@
+
+namespace ClasseEstatica{
+    public static class Calculadora{
+        public static int Somar(int a, int b){
+            return a+b;
+        }
+
+        public static double Somar(double a, double b){
+            return a+b;
+        }
+    }
+}
